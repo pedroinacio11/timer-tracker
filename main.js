8790c76d0022d306368731868aefd00c25df1ed8
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-09-06 01:13:38 -0300

    Estrutura básica do Electron --> CSS, JS e HTML

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const { app, BrowserWindow }  = require('electron');
 
+/* Iniciando a aplicação e abrindo a janela */
 app.on('ready', () => {
     console.log('Aplicação iniciada');
     let mainWindow = new BrowserWindow({
       width: 600,
       height: 400
     });
-
+    /* Passando o nosso index.html */
     mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 
+});
+
+/* Interrependo a aplicação corretamente */
+app.on('window-all-closed', () => {
+  app.quit();
 });
