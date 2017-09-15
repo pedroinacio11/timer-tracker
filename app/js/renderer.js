fcf6d8849581f83957a770e2e8e0f7481b1ac36e
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-09-14 23:26:34 -0300

    Ajuste

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,9 +1,11 @@
 /* Somente para manipulação do meu DOM */
-
 const { ipcRenderer } = require('electron');
+/* vou usar meu timer como se fosse um modulo node */
+const timer - require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
-let botaoPlay = document.querySelector('.botao-play')
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = documento.querySelector('.tempo');
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
@@ -14,6 +16,7 @@ let imgs = ['img/play-button.svg', 'img/stop-button.svg']
 botaoPlay.addEventListener('click', function(){
 
 /* Alterar minhas imagens play/stop */
-	imgs = imgs.reverse()
+	imgs = imgs.reverse();
+	timer.iniciar(tempo);
 	botaoPlay.src = imgs[0];
 });
--- app/js/timer.js
+++ app/js/timer.js
@@ -1,1 +1,18 @@
 /* Vou usar só para o Timer */
+const moment = require('moment');
+
+module.exports = {
+	iniciar(el){
+		console.log(el);
+		console.log(el.textContent);
+		setInterval(function(){
+			let tempo = moment.duration();
+		}, 1000);
+	}
+}
+
+
+	parar(){
+
+	},
+}
