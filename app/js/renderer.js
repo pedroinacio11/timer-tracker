13345f259b7c72a5677d13b0049700f0207d427e
pedro-inacio11 <pedro.inacio11@gmail.com>
2017-09-15 00:16:16 -0300

    Implementação da Função Parar() e Iniciar()

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,11 +1,11 @@
 /* Somente para manipulação do meu DOM */
 const { ipcRenderer } = require('electron');
 /* vou usar meu timer como se fosse um modulo node */
-const timer - require('./timer');
+const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
 let botaoPlay = document.querySelector('.botao-play');
-let tempo = documento.querySelector('.tempo');
+let tempo = document.querySelector('.tempo');
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
@@ -13,10 +13,17 @@ linkSobre.addEventListener('click' , function(){
 
 
 let imgs = ['img/play-button.svg', 'img/stop-button.svg']
-botaoPlay.addEventListener('click', function(){
+let play = false;
+botaoPlay.addEventListener('click', function() {
 
+	if(play == true){
+		timer.parar();
+		play = false;
+	}else{
+		timer.iniciar(tempo);
+		play = true;
+	}
 /* Alterar minhas imagens play/stop */
 	imgs = imgs.reverse();
-	timer.iniciar(tempo);
 	botaoPlay.src = imgs[0];
 });
--- app/js/timer.js
+++ app/js/timer.js
@@ -1,18 +1,22 @@
 /* Vou usar só para o Timer */
 const moment = require('moment');
+let segundos;
+let timer;
 
 module.exports = {
 	iniciar(el){
-		console.log(el);
-		console.log(el.textContent);
-		setInterval(function(){
-			let tempo = moment.duration();
+		let tempo = moment.duration(el.textContent);
+		segundos = tempo.asSeconds();
+		clearInterval(timer);
+		/* usei a arrowFunction para o this ficar fora do escopo e ser 
+		   usado no segundosParaTempo. Em uma Funtion não funcionaria */
+		timer = setInterval(()=>{
+			segundos++;
+			el.textContent = this.segundosParaTempo(segundos);
 		}, 1000);
+	},parar(){
+		clearInterval(timer);
+	},segundosParaTempo(segundos){
+		return moment().startOf('day').seconds(segundos).format("HH:mm:ss");
 	}
-}
-
-
-	parar(){
-
-	},
 }
